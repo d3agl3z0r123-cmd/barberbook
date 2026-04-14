ee21fb3f4b5ba37923dd5fd59d35a0b7228535da
d3agl3z0r123-cmd <d3agl3z0r123@gmail.com>
2026-04-14 00:59:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from twilio.rest import Client
 
 # 🔑 TWILIO CONFIG
 account_sid = "ACf8bcfa107ea60ff92a1aa0f9847dfb27"
-auth_token = st.secrets["TWILIO_AUTH_TOKEN"]
+auth_token = st.secrets["6LMNNLUR7VXB8QTVZVMFYC7K"]
 twilio_number = "+17406699179"
 
 client = Client(account_sid, auth_token)
